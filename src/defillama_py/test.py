b3de15f8de28eda9b31c0d79e354006d1c25d3de
Landon Gingerich <Landon>
2023-08-20 22:39:42 -0500

    basic testing

--- src/defillama_py/test.py
+++ src/defillama_py/test.py
@@ -3,15 +3,31 @@ from client import Llama
 # create a DefiLlama instance
 obj = Llama()
 
-# get historical DeFi TVL on all chains
-# print(obj.get_all_protocols_current_tvl())
+# df = obj.get_chains()
+# df = obj.get_protocols()
+# df = obj.get_stablecoins()
+# df = obj.get_pools()
 
-# df = obj.get_protocol_current_tvl('velodrome')
-# df = obj.get_all_protocols_current_tvl()
-# df = obj.get_protocol_historical_tvl('velodrome')
-# df = obj.get_chain_historical_tvl('arbitrum')
-# df = obj.get_all_chains_current_tvl()
+# df = obj.get_protocol_current_tvl(['velodrome'])
+# df = obj.get_all_protocols_current_tvl(raw=False)
+
+# df = obj.get_protocol_historical_tvl('satori-finance', raw=True)
+# df = obj.get_protocol_historical_tvl(['satori-finance','velodrome'], raw=True)
+# df = obj.get_protocol_historical_tvl(['satori-finance'], raw=False)
+# df = obj.get_protocol_historical_tvl(['satori-finance','velodrome'], raw=False)
+
+# df = obj.get_chain_historical_tvl('arbitrum', raw=True)
+# df = obj.get_chain_historical_tvl(['arbitrum', 'zkSync Era'], raw=True)
+# df = obj.get_chain_historical_tvl('arbitrum', raw=False)
+# df = obj.get_chain_historical_tvl(['arbitrum', 'zkSync Era'], raw=False)
+
+# df = obj.get_all_chains_current_tvl(raw=True)
+# df = obj.get_all_chains_current_tvl(raw=False)
+
 # df = obj.get_all_chains_historical_tvl()
+
+
+
 
 
 
